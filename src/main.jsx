43cdaddce3df5a88cdcf5fc3388074f202104e4c
Baptiste Desharbes <desharbesbaptiste@hotmail.fr>
2024-12-16 17:09:16 +0100

    light and dark mode done

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProviderWrapper } from "./context/auth.context.jsx";
 import "./index.css";
 import App from "./App.jsx";
+import { ThemeWrapper } from "./context/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProviderWrapper>
-        <App />
-      </AuthProviderWrapper>
-    </BrowserRouter>
+    <ThemeWrapper>
+      <BrowserRouter>
+        <AuthProviderWrapper>
+          <App />
+        </AuthProviderWrapper>
+      </BrowserRouter>
+    </ThemeWrapper>
   </StrictMode>
 );
