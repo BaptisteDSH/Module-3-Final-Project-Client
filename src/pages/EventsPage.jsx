137be6e01e70964b66e01da9f4508f3870d00581
Emilia Junghanns <emijunghanns@hotmail.com>
2024-12-16 14:43:10 +0100

    some css

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -29,22 +29,22 @@ const EventsPage = () => {
   return (
     <>
       <div className="events-page-container">
-        <div className="image-container">
+        <div className="event-image-container">
           <img src="https://www.educateurcaninfrance.com/wp-content/uploads/2024/05/GettyImages-1317531965-1.jpg" />
-          <div className="text-overlay">
+          <div className="event-text-overlay">
             <h1>It's Pawty time !</h1>
           </div>
         </div>
         <div className="button-add-event-container">
-          <h2>Are you the organiser ?</h2>
-          <h4>Click here to start !</h4>
+          <h2>Are you the organiser?</h2>
+          <h4>Click below to add a new event!</h4>
           <div>
             <Link to="/events/create">
               <div className="log-button">Add an event</div>
             </Link>
           </div>
         </div>
-        <div className="search-bar">SEARCH BAR TO CREATE</div>
+        {/* <div className="search-bar">SEARCH BAR TO CREATE</div> */}
         <div className="event-container">
           <div>
             <div className="event-box-container">
